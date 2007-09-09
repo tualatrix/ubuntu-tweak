f9e0e57363180184db1d3b80c67197dd2570f8bf
tualatrix <tualatrix@89e9d5ea-4437-0410-aedb-dd2c2f29ea1f>
2007-09-09 03:55:24 +0000

    把美工加上去了
    git-svn-id: https://ubuntu-tweak.googlecode.com/svn/trunk@29 89e9d5ea-4437-0410-aedb-dd2c2f29ea1f

--- src/about.c
+++ src/about.c
@@ -7,11 +7,13 @@
 
 void show_about(GtkWidget *widget,gpointer data)
 {
-	gchar*  authors[] = {"TualatriX <tualatrix@gmail.com>",NULL}; 
+	gchar* authors[] = {"TualatriX <tualatrix@gmail.com>",NULL}; 
+	gchar* artists[] = {"Wei Yao <a790407@hotmail.com> 3D Ubuntu Logo",NULL};
 
 	gtk_show_about_dialog(NULL,
 	"name","Ubuntu Tweak",
 	"authors", authors,
+	"artists", artists,
 	"website", "http://linuxdesktop.cn",
 	"copyright", "Copyright © 2007 TualatriX",
 	"comments", _("Ubuntu Tweak is tool for Ubuntu that makes it easy to config your system and desktop."),
