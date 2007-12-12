2f27a0852bbfe7f54485408977deba11ab7fc80e
tualatrix <tualatrix@89e9d5ea-4437-0410-aedb-dd2c2f29ea1f>
2007-12-12 02:52:40 +0000

    fixed the bugs can't run on 7.04 and 7.10
    git-svn-id: https://ubuntu-tweak.googlecode.com/svn/trunk@108 89e9d5ea-4437-0410-aedb-dd2c2f29ea1f

--- ubuntu-tweak.py
+++ ubuntu-tweak.py
@@ -8,7 +8,7 @@ from Computer import DISTRIB
 gettext.install("ubuntu-tweak", unicode = True)
 
 if __name__ == "__main__":
-	if DISTRIB != "feisty" or DISTRIB != "gutsy":
+	if DISTRIB != "feisty" and DISTRIB != "gutsy":
 		dialog = gtk.MessageDialog(None, gtk.DIALOG_MODAL, gtk.MESSAGE_ERROR, gtk.BUTTONS_OK)
 		dialog.set_title("Error")
 		dialog.set_markup(_("Sorry!\n\nUbuntu Tweak can only run on <b>Ubuntu 7.04 or 7.10.</b>\n"))
