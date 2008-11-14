764a9e0377b7e33ea3c974c3b971e9847b64199b
tualatrix <tualatrix@89e9d5ea-4437-0410-aedb-dd2c2f29ea1f>
2008-11-14 11:23:57 +0000

    Ubuntu Tweak 0.4.2 Final
    git-svn-id: https://ubuntu-tweak.googlecode.com/svn/trunk@397 89e9d5ea-4437-0410-aedb-dd2c2f29ea1f

--- src/sourceeditor.py
+++ src/sourceeditor.py
@@ -241,13 +241,10 @@ class SourceView(gtk.TextView):
 
         buffer.delete(buffer.get_start_iter(), buffer.get_end_iter())
         iter = buffer.get_iter_at_offset(0)
-        print '!!%s!!' % content
         if content[-2:] == '\n\n':
             content = content[:-1]
         for i, line in enumerate(content.split('\n')):
-            print i, '~%s~' % line
-#            if i != content.count('\n') and line:
-            self.parse_and_insert(buffer, iter, line)
+            self.parse_and_insert(buffer, iter, line, i != content.count('\n'))
 
         iter = buffer.get_iter_at_offset(offset)
         buffer.place_cursor(iter)
@@ -261,17 +258,21 @@ class SourceView(gtk.TextView):
 
         for i, line in enumerate(content.split('\n')):
             if i != content.count('\n') and line:
-                self.parse_and_insert(buffer, iter, line)
+                self.parse_and_insert(buffer, iter, line, i != content.count('\n'))
 
-    def parse_and_insert(self, buffer, iter, line):
+    def parse_and_insert(self, buffer, iter, line, break_line = False):
         try:
             if line.lstrip().startswith('#'):
                 buffer.insert_with_tags_by_name(iter, line, 'full_comment')
                 self.insert_line(buffer, iter)
+            elif line.strip() == '':
+                self.insert_line(buffer, iter)
             else:
                 has_end_blank = line.endswith(' ')
                 list = line.split()
-                if has_end_blank:
+                if list is None:
+                    self.insert_line(buffer, iter)
+                elif has_end_blank:
                     list[-1] = list[-1] + ' '
                 if len(list) > 3:
                     type, uri, distro, component = list[0], list[1], list[2], list[3:]
@@ -283,9 +284,8 @@ class SourceView(gtk.TextView):
                     buffer.insert_with_tags_by_name(iter, distro, 'distro')
                     self.insert_blank(buffer, iter)
                     self.seprarte_component(buffer, component, iter)
-                    buffer.insert(iter, '\n')
-                elif line == '':
-                    buffer.insert(iter, '\n')
+                    if break_line:
+                        self.insert_line(buffer, iter)
                 else:
                     buffer.insert(iter, line)
         except:
@@ -322,7 +322,7 @@ class SourceView(gtk.TextView):
 
         buffer.insert_with_tags_by_name(iter, ' '.join(component), 'component')
         if has_comment:
-            print '!%s!' % ' '.join(list[stop_i:])
+            self.insert_blank(buffer, iter)
             buffer.insert_with_tags_by_name(iter, ' '.join(list[stop_i:]), 'addon_comment')
 
     def get_text(self):
