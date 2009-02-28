7b5661da71c07c67b9cf598e8d2677c7e575c66a
TualatriX <tualatrix@tualatrix-desktop.(none)>
2009-02-28 21:10:26 +0800

    Fix dist check in third-party

--- src/thirdsoft.py
+++ src/thirdsoft.py
@@ -141,16 +141,26 @@ SOURCES_DATA = [
     ['http://ppa.launchpad.net/team-xbmc/ubuntu', ['hardy', 'intrepid'], 'main', XBMC],
 ]
 
+def is_ubuntu(distro):
+    if type(distro) == list:
+        for dis in distro:
+            if dis in module_check.get_supported_ubuntu():
+                return True
+            return False
+    else:
+        if distro in module_check.get_supported_ubuntu():
+            return True
+        return False
+
 def filter_sources():
     newsource = []
     for item in SOURCES_DATA:
         distro = item[1]
-        if type(distro) ==  type([]):
+        if is_ubuntu(distro):
             if module_check.get_codename() in distro:
                 newsource.append([item[0], module_check.get_codename(), item[2], item[3]])
         else:
-            if distro not in ['hardy', 'intrepid', 'jaunty']:
-                newsource.append(item)
+            newsource.append(item)
 
     return newsource
 
