af61c101a56bb73b92ed0e82f1ee6a473e39d229
TualatriX <tualatrix@gmail.com>
2009-05-06 20:01:08 +0800

    Clean the code of lockdown.py

--- src/lockdown.py
+++ src/lockdown.py
@@ -18,13 +18,6 @@
 # along with Ubuntu Tweak; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 
-import pygtk
-pygtk.require("2.0")
-import gtk
-import os
-import gconf
-import gettext
-
 from common.factory import WidgetFactory
 from common.widgets import ListPack, TweakPage
 
@@ -34,24 +27,24 @@ class LockDown(TweakPage):
         TweakPage.__init__(self)
 
         box = ListPack(_("System Security options"), (
-                    WidgetFactory.create("GconfCheckButton", 
-                                         label = _("Disable \"Run Application\" dialog (Alt+F2)"), 
-                                         key = "disable_command_line"),
                     WidgetFactory.create("GconfCheckButton",
-                                         label = _('Disable "Lock Screen"'), 
-                                         key = "disable_lock_screen"),
-                    WidgetFactory.create("GconfCheckButton", 
-                                         label = _("Disable printing"), 
-                                         key = "disable_printing"),
-                    WidgetFactory.create("GconfCheckButton", 
-                                         label = _("Disable print setup"), 
-                                         key = "disable_print_setup"),
-                    WidgetFactory.create("GconfCheckButton", 
-                                         label = _("Disable save to disk"), 
-                                         key = "disable_save_to_disk"),
-                    WidgetFactory.create("GconfCheckButton", 
-                                         label = _('Disable "Fast User Switching"'), 
-                                         key = "disable_user_switching"),
+                                         label=_("Disable \"Run Application\" dialog (Alt+F2)"),
+                                         key="disable_command_line"),
+                    WidgetFactory.create("GconfCheckButton",
+                                         label=_('Disable "Lock Screen"'),
+                                         key="disable_lock_screen"),
+                    WidgetFactory.create("GconfCheckButton",
+                                         label=_("Disable printing"),
+                                         key="disable_printing"),
+                    WidgetFactory.create("GconfCheckButton",
+                                         label=_("Disable print setup"),
+                                         key="disable_print_setup"),
+                    WidgetFactory.create("GconfCheckButton",
+                                         label=_("Disable save to disk"),
+                                         key="disable_save_to_disk"),
+                    WidgetFactory.create("GconfCheckButton",
+                                         label=_('Disable "Fast User Switching"'),
+                                         key="disable_user_switching"),
             ))
 
         self.pack_start(box, False, False, 0)
