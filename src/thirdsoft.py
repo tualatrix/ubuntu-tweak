fae0a193d587bb9fc2354855c3e6c13c99ad25f2
TualatriX <tualatrix@gmail.com>
2009-08-24 20:51:54 +0800

    Fix the typo again!

--- src/thirdsoft.py
+++ src/thirdsoft.py
@@ -498,9 +498,9 @@ class SourcesView(gtk.TreeView):
             distro = distro + '/'
 
         if TweakSettings.get_separated_sources():
-            result = proxy.set_separated_entry(url, distro, comps, comment, not enable, package)
+            result = proxy.set_separated_entry(url, distro, comps, comment, enable, package)
         else:
-            result = proxy.set_entry(url, distro, comps, comment, not enable)
+            result = proxy.set_entry(url, distro, comps, comment, enable)
 
         if str(result) == 'enabled':
             self.model.set(iter, COLUMN_ENABLED, True)
