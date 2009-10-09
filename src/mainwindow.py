3f5339cd00d6ffd96193e137e8a1fb125743d6e8
TualatriX <tualatrix@gmail.com>
2009-10-09 19:20:53 +0800

    Update contributors list

--- src/mainwindow.py
+++ src/mainwindow.py
@@ -600,14 +600,22 @@ class MainWindow(gtk.Window):
         about.set_website_label(_('Ubuntu Tweak Website'))
         about.set_logo(self.get_icon())
         about.set_comments(_("Ubuntu Tweak is a tool for Ubuntu that makes it easy to configure your system and desktop settings."))
-        about.set_authors(["TualatriX tualatrix@gmail.com", "Lee Jarratt lee.jarratt@googlemail.com (English consultants)"])
+        about.set_authors(["TualatriX <tualatrix@gmail.com>", "",
+            _("Contributors of 2007"),
+            "Super Jamie <jamie@superjamie.net>", "",
+            _("Contributors of 2008"),
+            "Lee Jarratt <lee.jarratt@googlemail.com>", "",
+            _("Contributors of 2009"),
+            "Iven <ivenvd@gmail.com>",
+            "Dig Ge <Dig.Ge.CN@Gmail.com>",
+            ])
         about.set_copyright("Copyright © 2007-2009 TualatriX")
         about.set_wrap_license(True)
         about.set_license("Ubuntu Tweak is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.\n\
 Ubuntu Tweak is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\n\
 You should have received a copy of the GNU General Public License along with Ubuntu Tweak; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA")
         about.set_translator_credits(_("translator-credits"))
-        about.set_artists(["m.Sharp mac.sharp@gmail.com Logo and Banner", "Medical-Wei a790407@hotmail.com ArtWork of 0.1 version"])
+        about.set_artists(["m.Sharp <mac.sharp@gmail.com> Logo and Banner", "Medical-Wei <a790407@hotmail.com> Artwork of 0.1 version"])
         about.run()
         about.destroy()
 
