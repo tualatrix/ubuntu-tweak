70fef8e87be9a98acc29a4361a67a6c35aef9c0f
TualatriX <tualatrix@gmail.com>
2009-11-18 22:39:02 +0800

    Update description of task install

--- ubuntutweak/modules/taskinstall.py
+++ ubuntutweak/modules/taskinstall.py
@@ -159,7 +159,7 @@ class TaskView(gtk.TreeView):
 
 class TaskInstall(TweakModule):
     __title__ = _('Task Install')
-    __desc__ = _('Setup a full-function environment in just one-click')
+    __desc__ = _('Setup a full-function environment with just one-click\nIf you want to remove a task, click the "Installed" button')
     __icon__ = ['application-x-deb']
     __category__ = 'system'
 
