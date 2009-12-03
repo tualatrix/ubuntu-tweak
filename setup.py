8cdfde109339c261e6d34b3038bf9f380284265a
TualatriX <tualatrix@gmail.com>
2009-12-03 22:21:49 +0800

    Bump version to 20091203

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import glob
 from setuptools import *
 
 setup(name='ubuntu-tweak',
-      version='0.4.999.20091128',
+      version='0.4.999.20091203',
       description='magic tool to configure Ubuntu',
       author='TualatriX',  
       author_email='tualatrix@gmail.com',
