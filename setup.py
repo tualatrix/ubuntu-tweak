ea67b4939d6e2376a73da73d9dc756ac3d75c758
TualatriX <tualatrix@gmail.com>
2010-03-01 20:29:29 +0800

    Bump version to 0.5.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import glob
 from setuptools import *
 
 setup(name='ubuntu-tweak',
-      version='0.5.1',
+      version='0.5.2',
       description='magic tool to configure Ubuntu',
       author='TualatriX',  
       author_email='tualatrix@gmail.com',
--- ubuntutweak/common/consts.py
+++ ubuntutweak/common/consts.py
@@ -14,7 +14,7 @@ def applize(package):
     return ' '.join([a.capitalize() for a in package.split('-')])
 
 PACKAGE = 'ubuntu-tweak'
-VERSION = '0.5.1'
+VERSION = '0.5.2'
 DATA_DIR = '/usr/share/ubuntu-tweak/'
 APP = applize(PACKAGE)
 CONFIG_ROOT = os.path.join(glib.get_user_config_dir(), 'ubuntu-tweak')
