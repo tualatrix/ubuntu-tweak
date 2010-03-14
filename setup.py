c627cc119efad21d021034e7588c4834a204b443
TualatriX <tualatrix@gmail.com>
2010-03-14 13:42:41 +0800

    Bump version to 0.5.3, released

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import glob
 from setuptools import *
 
 setup(name='ubuntu-tweak',
-      version='0.5.2',
+      version='0.5.3',
       description='magic tool to configure Ubuntu',
       author='TualatriX',  
       author_email='tualatrix@gmail.com',
