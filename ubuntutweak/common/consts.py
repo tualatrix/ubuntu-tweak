b7198def3e9b516c911ea0a8f5448669325f5fca
TualatriX <tualatrix@gmail.com>
2010-07-27 22:01:34 +0800

    Bump version to 0.5.6 and make the fakeroot build pass

--- ubuntutweak/common/consts.py
+++ ubuntutweak/common/consts.py
@@ -16,13 +16,14 @@ def applize(package):
     return ' '.join([a.capitalize() for a in package.split('-')])
 
 PACKAGE = 'ubuntu-tweak'
-VERSION = '0.5.5'
+VERSION = '0.5.6'
 DATA_DIR = '/usr/share/ubuntu-tweak/'
 APP = applize(PACKAGE)
 CONFIG_ROOT = os.path.join(glib.get_user_config_dir(), 'ubuntu-tweak')
 IS_INSTALLED = True
 
-if not os.path.exists(CONFIG_ROOT):
+# The os.access is hack for fakeroot deb build
+if not os.path.exists(CONFIG_ROOT) and os.access(glib.get_user_config_dir(), os.W_OK):
     os.makedirs(CONFIG_ROOT)
 
 try:
