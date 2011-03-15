17f464917a8adfdd801a0bdb01657ca2e72623dd
Tualatrix Chou <tualatrix@gmail.com>
2011-03-15 16:31:06 +0800

    Add the new WelcomePage

--- ubuntutweak/app.py
+++ ubuntutweak/app.py
@@ -16,14 +16,55 @@
 # along with Ubuntu Tweak; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 
-from gi.repository import Gtk, Unique
+import gobject
+from gi.repository import Gtk, Unique, Pango
 
 from ubuntutweak.gui import GuiBuilder
+
+
+class WelcomePage(Gtk.VBox):
+    def __init__(self):
+        gobject.GObject.__init__(self)
+
+        self.set_border_width(20)
+
+        title = Gtk.MenuItem(label='')
+        label = title.get_child()
+        label.set_markup(_('\n<span size="xx-large">Welcome to <b>Ubuntu Tweak!</b></span>\n'))
+        label.set_alignment(0.5, 0.5)
+        title.select()
+        self.pack_start(title, False, False, 10)
+
+        tips = self.create_tips(
+                _('Tweak otherwise hidden settings.'),
+                _('Clean up unused packages to free up diskspace.'),
+                _('Easily install up-to-date versions of many applications.'),
+                _('Configure file templates and shortcut scripts for easy access to common tasks.'),
+                _('Many more useful features!'),
+                )
+
+    def create_tips(self, *tips):
+        for tip in tips:
+            hbox = Gtk.HBox()
+            image = Gtk.Image.new_from_stock(Gtk.STOCK_GO_FORWARD,
+                                             Gtk.IconSize.BUTTON)
+            hbox.pack_start(image, False, False, 15)
+
+            label = Gtk.Label()
+            label.set_alignment(0.0, 0.5)
+            label.set_ellipsize(Pango.EllipsizeMode.END)
+            label.set_markup(tip)
+            hbox.pack_start(label, True, True, 0)
+
+            self.pack_start(hbox, False, False, 10)
+
 
 class UbuntuTweakApp(Unique.App, GuiBuilder):
     def __init__(self, name='com.ubuntu-tweak.main', startup_id=''):
         Unique.App.__init__(self, name=name, startup_id=startup_id)
         GuiBuilder.__init__(self, file_name='mainwindow.ui')
+
+        self.tweaknotebook.append_page(WelcomePage(), Gtk.Label(label=_('Welcome')))
 
         self.watch_window(self.mainwindow)
         self.connect('message-received', self.on_message_received)
