c6773377cbe99ac59c8acb86b98741bf206b4ecb
Tualatrix Chou <tualatrix@gmail.com>
2011-09-20 17:07:26 +0800

    Enable the metacity.py for Oneiric again

--- ubuntutweak/tweaks/metacity.py
+++ ubuntutweak/tweaks/metacity.py
@@ -148,7 +148,6 @@ class Metacity(TweakModule):
     __icon__ = 'preferences-system-windows'
     __url__ = 'http://ubuntu-tweak.com'
     __category__ = 'desktop'
-    __distro__ = 'natty'
 
     left_default = 'close,minimize,maximize:'
     right_default = ':minimize,maximize,close'
