5a0186a90778daa97af7a57f9da7ab743fef8a7d
Tualatrix Chou <tualatrix@gmail.com>
2011-10-04 23:02:32 +0800

    Session settings now supports Oneiric

--- ubuntutweak/tweaks/session.py
+++ ubuntutweak/tweaks/session.py
@@ -18,7 +18,7 @@
 
 import os
 
-from gi.repository import Gtk, GConf
+from gi.repository import Gtk, Gio
 
 from ubuntutweak.modules  import TweakModule
 from ubuntutweak.factory import WidgetFactory
@@ -30,94 +30,45 @@ class Session(TweakModule):
     __desc__ = _('Control your system session releated features')
     __icon__ = 'gnome-session-hibernate'
     __category__ = 'startup'
-    __distro__ = 'natty'
 
     def __init__(self):
         TweakModule.__init__(self)
 
-        data = {
-            'changed': self.on_entry_changed,
-        }
-        label1, entry1, reset1 = WidgetFactory.create('Entry',
-            label=_('File Manager'),
-            key='/desktop/gnome/session/required_components/filemanager',
-            enable_reset=True,
-            backend=GConf,
-            signal_dict=data)
-        label2, entry2, reset2 = WidgetFactory.create('Entry',
-            label=_('Panel'),
-            enable_reset=True,
-            signal_dict=data,
-            backend=GConf,
-            key='/desktop/gnome/session/required_components/panel')
-        label3, entry3, reset3 = WidgetFactory.create('Entry',
-            label=_('Window Manager'),
-            enable_reset=True,
-            signal_dict=data,
-            backend=GConf,
-            key='/desktop/gnome/session/required_components/windowmanager')
-
-        hbox1 = Gtk.HBox(spacing=12)
-        self.apply_button = Gtk.Button(stock=Gtk.STOCK_APPLY)
-        self.apply_button.changed_table = {}
-        self.apply_button.set_sensitive(False)
-        self.apply_button.connect('clicked', self.on_apply_clicked, (entry1, entry2, entry3))
-        hbox1.pack_end(self.apply_button, False, False, 0)
-
-        table = TablePack(_('Session Control'), (
-                    (label1, entry1, reset1),
-                    (label2, entry2, reset2),
-                    (label3, entry3, reset3),
-                    hbox1,
-                ))
-
-        self.add_start(table, False, False, 0)
-
-        box = ListPack(_("Session Options"), (
+        box = ListPack(_("Session Menu Options"), (
                   WidgetFactory.create("CheckButton",
-                      label=_("Suppress the logout, restart and shutdown confirmation dialogue box."),
+                      label=_("Remove the log out item from the session menu"),
                       enable_reset=True,
-                      backend=GConf,
-                      key="/apps/indicator-session/suppress_logout_restart_shutdown"),
+                      backend=Gio,
+                      key="com.canonical.indicator.session.suppress-logout-menuitem"),
                   WidgetFactory.create("CheckButton",
-                      label=_("Remove the Log Out item from the session menu"),
+                      label=_("Remove the restart item from the session menu"),
                       enable_reset=True,
-                      backend=GConf,
-                      key="/apps/indicator-session/suppress_logout_menuitem"),
-                  WidgetFactory.create("CheckButton",
-                      label=_("Remove the Restart item from the session menu"),
-                      enable_reset=True,
-                      backend=GConf,
-                      key="/apps/indicator-session/suppress_restart_menuitem"),
+                      backend=Gio,
+                      key="com.canonical.indicator.session.suppress-restart-menuitem"),
                   WidgetFactory.create("CheckButton",
                       label=_("Remove the shutdown item from the session menu"),
                       enable_reset=True,
-                      backend=GConf,
-                      key="/apps/indicator-session/suppress_shutdown_menuitem"),
+                      backend=Gio,
+                      key="com.canonical.indicator.session.suppress-shutdown-menuitem"),
+                  WidgetFactory.create("CheckButton",
+                      label=_("Suppress the dialog to confirm logout, restart and shutdown action"),
+                      enable_reset=True,
+                      backend=Gio,
+                      key="com.canonical.indicator.session.suppress-logout-restart-shutdown"),
                 ))
 
         self.add_start(box, False, False, 0)
 
-    def on_entry_changed(self, widget):
-        if widget.is_changed():
-            self.apply_button.changed_table[widget] = True
-        else:
-            self.apply_button.changed_table.pop(widget, None)
-        self.set_apply_button_sensitive()
-
-    def set_apply_button_sensitive(self):
-        if self.apply_button.changed_table.items():
-            self.apply_button.set_sensitive(True)
-        else:
-            self.apply_button.set_sensitive(False)
-
-    def on_apply_clicked(self, widget, entrys):
-        for entry in entrys:
-            if entry.is_changed() and entry.get_text().decode('utf-8') != _('Unset'):
-                if os.popen('which %s' % entry.get_text()).read():
-                    entry.on_edit_finished_cb(entry)
-                else:
-                    ErrorDialog(title=_('"%s" is not available.') % entry.get_text(),
-                                message=_('Please enter a valid application command line.')).launch()
-                    entry.reset()
-        self.apply_button.set_sensitive(False)
+        box = ListPack(_("User Menu Options"), (
+                    WidgetFactory.create("CheckButton",
+                                    label=_('Show user menu'),
+                                    enable_reset=True,
+                                    backend=Gio,
+                                    key='com.canonical.indicator.session.user-show-menu'),
+                    WidgetFactory.create("CheckButton",
+                                    label=_("Show user's real name on the panel"),
+                                    enable_reset=True,
+                                    backend=Gio,
+                                    key="com.canonical.indicator.session.show-real-name-on-panel"),
+            ))
+        self.add_start(box, False, False, 0)
