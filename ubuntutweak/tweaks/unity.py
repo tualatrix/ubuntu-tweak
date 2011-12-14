61263ef32d7cf514e1ced9c865b9c352b13c3937
Tualatrix Chou <tualatrix@gmail.com>
2011-12-14 09:27:47 +0800

    Unity Settings doesn't work under unityshell-rotated

--- ubuntutweak/tweaks/unity.py
+++ ubuntutweak/tweaks/unity.py
@@ -16,6 +16,8 @@
 # along with Ubuntu Tweak; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 
+import os
+
 from gi.repository import Gtk, Gio
 
 from ubuntutweak.gui.containers import ListPack, TablePack
@@ -33,117 +35,124 @@ class UnitySettings(TweakModule):
     def __init__(self):
         TweakModule.__init__(self)
 
-        if system.DESKTOP == 'ubuntu':
-            box = TablePack(_("Launcher"), (
-                        WidgetFactory.create("Scale",
-                                             label=_('Icon size'),
-                                             key="unityshell.icon_size",
-                                             min=32,
-                                             max=64,
-                                             backend="compiz",
-                                             enable_reset=True),
-                        WidgetFactory.create("Scale",
-                                             label=_('Launcher opacity'),
-                                             key="unityshell.launcher_opacity",
-                                             min=0,
-                                             max=1,
-                                             digits=2,
-                                             backend="compiz",
-                                             enable_reset=True),
-                        WidgetFactory.create("Scale",
-                                             label=_('Launcher reveal edge timeout'),
-                                             key="unityshell.launcher_reveal_edge_timeout",
-                                             min=1,
-                                             max=1000,
-                                             backend="compiz",
-                                             enable_reset=True),
-                        WidgetFactory.create("ComboBox",
-                                             label=_('Launcher hide mode'),
-                                             key="unityshell.launcher_hide_mode",
-                                             texts=(_('Never'), _('Auto Hide'),
-                                                    _('Dodge Window'), _('Dodge Active Window')),
-                                             values=(0, 1, 2, 3),
-                                             type=int,
-                                             backend="compiz",
-                                             enable_reset=True),
-                        WidgetFactory.create("ComboBox",
-                                             label=_('Icon backlight'),
-                                             key="unityshell.backlight_mode",
-                                             texts=(_('Backlight Always On'),
-                                                    _('Backlight Toggles'),
-                                                    _('Backlight Always Off'),
-                                                    _('Edge Illumination Toggles'),
-                                                    _('Backlight and Edge Illumination Toggles')),
-                                             values=(0, 1, 2, 3, 4),
-                                             type=int,
-                                             backend="compiz",
-                                             enable_reset=True),
-                ))
-            self.add_start(box, False, False, 0)
+        wrong_unity_pkg = 'unityshell-rotated'
 
-            box = TablePack(_("Dash and Panel"), (
-                        WidgetFactory.create("ComboBox",
-                                             label=_('Dash size'),
-                                             key="com.canonical.Unity.form-factor",
-                                             texts=(_('Automatic'),
-                                                    _('Desktop'),
-                                                    _('Netbook')),
-                                             values=('Automatic',
-                                                     'Desktop',
-                                                     'Netbook'),
-                                             backend="gsettings",
-                                             enable_reset=True),
-                        WidgetFactory.create("ComboBox",
-                                             label=_('Blur'),
-                                             key="unityshell.dash_blur_experimental",
-                                             texts=(_('No blur'),
-                                                    _('Static blur'),
-                                                    _('Active blur')),
-                                             values=(0, 1, 2),
-                                             type=int,
-                                             backend="compiz",
-                                             enable_reset=True),
-                        WidgetFactory.create("Scale",
-                                             label=_('Panel opacity'),
-                                             key="unityshell.panel_opacity",
-                                             min=0,
-                                             max=1,
-                                             digits=2,
-                                             backend="compiz",
-                                             enable_reset=True),
-                ))
-
-            self.add_start(box, False, False, 0)
+        if wrong_unity_pkg in os.popen("dpkg-query -f '${Version}' -W %s" % wrong_unity_pkg).read():
+            label = Gtk.Label()
+            label.set_markup(_("<big>Sorry! Unity Settings doesn't support unityshell-rotated</big>"))
+            self.add_start(label, False, False, 0)
         else:
-            box = ListPack(_('Compositing Manager'), (
-                                    WidgetFactory.create('CheckButton',
-                                                         label=_("Enable Metacity's compositing feature"),
-                                                         enable_reset=True,
-                                                         backend='gconf',
-                                                         key='/apps/metacity/general/compositing_manager'),
-                            ))
-            self.add_start(box, False, False, 0)
+            if system.DESKTOP == 'ubuntu':
+                box = TablePack(_("Launcher"), (
+                            WidgetFactory.create("Scale",
+                                                 label=_('Icon size'),
+                                                 key="unityshell.icon_size",
+                                                 min=32,
+                                                 max=64,
+                                                 backend="compiz",
+                                                 enable_reset=True),
+                            WidgetFactory.create("Scale",
+                                                 label=_('Launcher opacity'),
+                                                 key="unityshell.launcher_opacity",
+                                                 min=0,
+                                                 max=1,
+                                                 digits=2,
+                                                 backend="compiz",
+                                                 enable_reset=True),
+                            WidgetFactory.create("Scale",
+                                                 label=_('Launcher reveal edge timeout'),
+                                                 key="unityshell.launcher_reveal_edge_timeout",
+                                                 min=1,
+                                                 max=1000,
+                                                 backend="compiz",
+                                                 enable_reset=True),
+                            WidgetFactory.create("ComboBox",
+                                                 label=_('Launcher hide mode'),
+                                                 key="unityshell.launcher_hide_mode",
+                                                 texts=(_('Never'), _('Auto Hide'),
+                                                        _('Dodge Window'), _('Dodge Active Window')),
+                                                 values=(0, 1, 2, 3),
+                                                 type=int,
+                                                 backend="compiz",
+                                                 enable_reset=True),
+                            WidgetFactory.create("ComboBox",
+                                                 label=_('Icon backlight'),
+                                                 key="unityshell.backlight_mode",
+                                                 texts=(_('Backlight Always On'),
+                                                        _('Backlight Toggles'),
+                                                        _('Backlight Always Off'),
+                                                        _('Edge Illumination Toggles'),
+                                                        _('Backlight and Edge Illumination Toggles')),
+                                                 values=(0, 1, 2, 3, 4),
+                                                 type=int,
+                                                 backend="compiz",
+                                                 enable_reset=True),
+                    ))
+                self.add_start(box, False, False, 0)
 
-            box = TablePack(_("Launcher"), (
-                        WidgetFactory.create("CheckButton",
-                                             label=_('Enable the Super key'),
-                                             key="com.canonical.Unity2d.Launcher.super-key-enable",
-                                             backend="gsettings",
-                                             enable_reset=True),
-                        WidgetFactory.create("CheckButton",
-                                             label=_('Reserve launcher area'),
-                                             key="com.canonical.Unity2d.Launcher.use-strut",
-                                             backend="gsettings",
-                                             enable_reset=True),
-                        WidgetFactory.create("ComboBox",
-                                             label=_('Launcher hide mode'),
-                                             key="com.canonical.Unity2d.Launcher.hide-mode",
-                                             texts=(_('Never'), _('Auto Hide'),
-                                                    _('Intellihide')),
-                                             values=(0, 1, 2),
-                                             type=int,
-                                             backend="gsettings",
-                                             enable_reset=True),
-                ))
+                box = TablePack(_("Dash and Panel"), (
+                            WidgetFactory.create("ComboBox",
+                                                 label=_('Dash size'),
+                                                 key="com.canonical.Unity.form-factor",
+                                                 texts=(_('Automatic'),
+                                                        _('Desktop'),
+                                                        _('Netbook')),
+                                                 values=('Automatic',
+                                                         'Desktop',
+                                                         'Netbook'),
+                                                 backend="gsettings",
+                                                 enable_reset=True),
+                            WidgetFactory.create("ComboBox",
+                                                 label=_('Blur'),
+                                                 key="unityshell.dash_blur_experimental",
+                                                 texts=(_('No blur'),
+                                                        _('Static blur'),
+                                                        _('Active blur')),
+                                                 values=(0, 1, 2),
+                                                 type=int,
+                                                 backend="compiz",
+                                                 enable_reset=True),
+                            WidgetFactory.create("Scale",
+                                                 label=_('Panel opacity'),
+                                                 key="unityshell.panel_opacity",
+                                                 min=0,
+                                                 max=1,
+                                                 digits=2,
+                                                 backend="compiz",
+                                                 enable_reset=True),
+                    ))
 
-            self.add_start(box, False, False, 0)
+                self.add_start(box, False, False, 0)
+            else:
+                box = ListPack(_('Compositing Manager'), (
+                                        WidgetFactory.create('CheckButton',
+                                                             label=_("Enable Metacity's compositing feature"),
+                                                             enable_reset=True,
+                                                             backend='gconf',
+                                                             key='/apps/metacity/general/compositing_manager'),
+                                ))
+                self.add_start(box, False, False, 0)
+
+                box = TablePack(_("Launcher"), (
+                            WidgetFactory.create("CheckButton",
+                                                 label=_('Enable the Super key'),
+                                                 key="com.canonical.Unity2d.Launcher.super-key-enable",
+                                                 backend="gsettings",
+                                                 enable_reset=True),
+                            WidgetFactory.create("CheckButton",
+                                                 label=_('Reserve launcher area'),
+                                                 key="com.canonical.Unity2d.Launcher.use-strut",
+                                                 backend="gsettings",
+                                                 enable_reset=True),
+                            WidgetFactory.create("ComboBox",
+                                                 label=_('Launcher hide mode'),
+                                                 key="com.canonical.Unity2d.Launcher.hide-mode",
+                                                 texts=(_('Never'), _('Auto Hide'),
+                                                        _('Intellihide')),
+                                                 values=(0, 1, 2),
+                                                 type=int,
+                                                 backend="gsettings",
+                                                 enable_reset=True),
+                    ))
+
+                self.add_start(box, False, False, 0)
