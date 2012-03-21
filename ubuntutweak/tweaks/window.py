925d47605c1cd9e66a3ec06a15f50a3b2c252173
Tualatrix Chou <tualatrix@gmail.com>
2012-03-21 20:55:22 +0800

    Clean up code in window, now work again

--- ubuntutweak/tweaks/window.py
+++ ubuntutweak/tweaks/window.py
@@ -1,6 +1,6 @@
 # Ubuntu Tweak - Ubuntu Configuration Tool
 #
-# Copyright (C) 2007-2011 Tualatrix Chou <tualatrix@gmail.com>
+# Copyright (C) 2007-2012 Tualatrix Chou <tualatrix@gmail.com>
 #
 # Ubuntu Tweak is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,131 +19,10 @@
 from gi.repository import GObject, Gtk
 
 from ubuntutweak.modules  import TweakModule
-from ubuntutweak.gui.dialogs import InfoDialog
-from ubuntutweak.gui.containers import ListPack, GridPack
+from ubuntutweak.gui.containers import GridPack
 from ubuntutweak.factory import WidgetFactory
 from ubuntutweak.settings.gconfsettings import GconfSetting
 from ubuntutweak import system
-
-
-class ButtonView(Gtk.IconView):
-    (COLUMN_VALUE,
-     COLUMN_LABEL,
-    ) = range(2)
-
-    values = {
-        'menu': _('Menu'),
-        ':': _('Title'),
-        'maximize': _('Maximize'),
-        'minimize': _('Minimize'),
-        'close': _('Close'),
-        'spacer': _('Spacer'),
-    }
-
-    if system.DESKTOP == 'gnome-shell':
-        config = GconfSetting(key='/desktop/gnome/shell/windows/button_layout')
-    else:
-        config = GconfSetting(key='/apps/metacity/general/button_layout')
-
-    def __init__(self):
-        GObject.GObject.__init__(self)
-
-        model = self._create_model()
-        self.set_model(model)
-        self.update_model()
-
-        self.set_text_column(self.COLUMN_LABEL)
-        self.set_reorderable(True)
-        self.set_item_orientation(Gtk.Orientation.HORIZONTAL)
-        self.connect('selection-changed', self.on_selection_changed)
-
-    def _create_model(self):
-        model = Gtk.ListStore(GObject.TYPE_STRING, GObject.TYPE_STRING)
-
-        return model
-
-    @classmethod
-    def get_control_items(cls):
-        dict = cls.values.copy()
-        dict.pop(':')
-        return dict.items()
-
-    @classmethod
-    def is_value(cls, value):
-        if value == cls.config.get_value():
-            return True
-        else:
-            return False
-
-    def update_model(self, default=None):
-        model = self.get_model()
-        model.clear()
-
-        value = default or self.config.get_value()
-        if value:
-            list = value.replace(':', ',:,').split(',')
-        else:
-            return model
-
-        for k in list:
-            k = k.strip()
-            if k in self.values:
-                model.append((k, self.values[k]))
-            else:
-                continue
-
-    def on_selection_changed(self, widget, data=None):
-        model = widget.get_model()
-        value = ','.join([i[self.COLUMN_VALUE] for i in model])
-        value = value.replace(',:', ':').replace(':,', ':')
-        self.config.set_value(value)
-
-    def add_button(self, value):
-        model = self.get_model()
-        model.append((value, self.values[value]))
-        self.on_selection_changed(self)
-
-    def remove_button(self, value):
-        model = self.get_model()
-        for i, row in enumerate(model):
-            if row[self.COLUMN_VALUE] == value:
-                model.remove(row.iter)
-        self.on_selection_changed(self)
-
-    def has_button(self, value):
-        model = self.get_model()
-        for i, row in enumerate(model):
-            if row[self.COLUMN_VALUE] == value:
-                return True
-        return False
-
-
-class WindowControlButton(Gtk.CheckButton):
-    '''The individual checkbutton to control window control'''
-
-    def __init__(self, label, value, view):
-        '''Init the button, the view must be the ButtonView'''
-        self._label = label
-        self._value = value
-        self._view = view
-
-        GObject.GObject.__init__(self, label=self._label)
-        self.set_active(self._view.has_button(self._value))
-
-        self._handle_id = self.connect('toggled', self.on_toggled)
-
-    def on_toggled(self, widget):
-        '''Emit extra signal when toggle button'''
-        if widget.get_active():
-            self._view.add_button(self._value)
-        else:
-            self._view.remove_button(self._value)
-
-    def update_status(self):
-        '''Update status is called from others, not user, so need to block the handler'''
-        self.handler_block(self._handle_id)
-        self.set_active(self._view.has_button(self._value))
-        self.handler_unblock(self._handle_id)
 
 
 class Window(TweakModule):
@@ -155,25 +34,13 @@ class Window(TweakModule):
     left_default = 'close,minimize,maximize:'
     right_default = ':minimize,maximize,close'
 
+    if system.DESKTOP == 'gnome-shell':
+        config = GconfSetting(key='/desktop/gnome/shell/windows/button_layout')
+    else:
+        config = GconfSetting(key='/apps/metacity/general/button_layout')
+
     def __init__(self):
         TweakModule.__init__(self, 'window.ui')
-
-        #TODO may clean up
-#        swindow = Gtk.ScrolledWindow()
-#        swindow.set_size_request(-1, 54)
-#        swindow.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
-#        self.buttonview = ButtonView()
-#        swindow.add(self.buttonview)
-#        self.vbox2.pack_start(swindow, False, False, 0)
-
-#        for value, label in ButtonView.get_control_items():
-#            button = WindowControlButton(label, value, self.buttonview)
-#            self.control_hbox.pack_start(button, False, False, 0)
-
-#        box = ListPack(_('Window Titlebar Button Layout'),
-#                       [child for child in self.main_vbox.get_children()])
-#        self.add_start(box, False, False, 0)
-#        self.init_control_buttons()
 
         box = GridPack(
                     (Gtk.Label(_('Window control button position')),
@@ -231,39 +98,10 @@ class Window(TweakModule):
 
         self.add_start(box)
 
-    def init_control_buttons(self):
-        if ButtonView.is_value(self.left_default):
-            self.left_radio.set_active(True)
-        elif ButtonView.is_value(self.right_default):
-            self.right_radio.set_active(True)
-        else:
-            self.place_hbox.set_sensitive(False)
-            self.custom_button.set_active(True)
-            self.custom_hbox.set_sensitive(True)
-
-    def on_custom_button_toggled(self, widget, data=None):
-        if not widget.get_active():
-            self.place_hbox.set_sensitive(True)
-            self.custom_button.set_active(False)
-            self.custom_hbox.set_sensitive(False)
-        else:
-            self.place_hbox.set_sensitive(False)
-            self.custom_button.set_active(True)
-            self.custom_hbox.set_sensitive(True)
-
-    def on_right_radio_toggled(self, widget, data=None):
+    def on_right_radio_toggled(self, widget):
         if widget.get_active():
-            self.buttonview.update_model(default=self.right_default)
-            self.buttonview.on_selection_changed(self.buttonview)
-            self.update_control_buttons()
+            self.config.set_value(self.right_default)
 
-    def on_left_radio_toggled(self, widget, data=None):
+    def on_left_radio_toggled(self, widget):
         if widget.get_active():
-            self.buttonview.update_model(default=self.left_default)
-            self.buttonview.on_selection_changed(self.buttonview)
-            self.update_control_buttons()
-
-    def update_control_buttons(self):
-        for button in self.control_hbox.get_children():
-            if type(button) == WindowControlButton:
-                button.update_status()
+            self.config.set_value(self.left_default)
