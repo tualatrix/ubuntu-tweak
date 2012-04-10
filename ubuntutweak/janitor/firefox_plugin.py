c0e337a64a88bf8686e2e7006128c1ad3de2a439
Tualatrix Chou <tualatrix@gmail.com>
2012-04-10 17:24:34 +0800

    Now use a common MozillaCachePlugin to support FirefoxCachePlugin and ThunderbirdCachePlugin

--- ubuntutweak/janitor/firefox_plugin.py
+++ ubuntutweak/janitor/firefox_plugin.py
@@ -3,15 +3,21 @@ import os
 from ubuntutweak.janitor import JanitorCachePlugin
 from ubuntutweak.settings.configsettings import RawConfigSetting
 
-class FirefoxCachePlugin(JanitorCachePlugin):
-    __title__ = _('Firefox Cache')
+class MozillaCachePlugin(JanitorCachePlugin):
     __category__ = 'application'
 
-    targets = ['Cache', 'OfflineCache', 'TestPilotErrorLog.log']
+    targets = ['Cache',
+               'OfflineCache',
+               'TestPilotErrorLog.log',
+               'downloads.sqlite',
+               'cookies.sqlite',
+               'cookies.sqlite-shm',
+               'cookies.sqlite-wal']
+    app_path = ''
 
     @classmethod
     def get_path(cls):
-        profiles_path = os.path.expanduser('~/.mozilla/firefox/profiles.ini')
+        profiles_path = os.path.expanduser('%s/profiles.ini' % cls.app_path)
         if os.path.exists(profiles_path):
             config = RawConfigSetting(profiles_path)
             profile_id = config.get_value('General', 'StartWithLastProfile')
@@ -19,6 +25,16 @@ class FirefoxCachePlugin(JanitorCachePlugin):
                 if section.startswith('Profile'):
                     relative_id = config.get_value(section, 'IsRelative')
                     if relative_id == profile_id:
-                        return os.path.expanduser('~/.mozilla/firefox/%s' %
-config.get_value(section, 'Path'))
-        return self.root_path
+                        return os.path.expanduser('%s/%s' % (cls.app_path, config.get_value(section, 'Path')))
+        return cls.root_path
+
+
+class FirefoxCachePlugin(MozillaCachePlugin):
+    __title__ = _('Firefox Cache')
+
+    app_path = '~/.mozilla/firefox'
+
+class ThunderbirdCachePlugin(MozillaCachePlugin):
+    __title__ = _('Thunderbird Cache')
+
+    app_path = '~/.thunderbird'
