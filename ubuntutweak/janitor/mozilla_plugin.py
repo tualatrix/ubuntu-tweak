2858f9fe4f25a8a3b43374ab91fac556e324a68e
Tualatrix Chou <tualatrix@gmail.com>
2014-05-04 18:32:03 +0800

    Fix Bug #1280540: Cache path of Mozilla Firefox and Thunderbird changed to ~/.cache

--- ubuntutweak/janitor/mozilla_plugin.py
+++ ubuntutweak/janitor/mozilla_plugin.py
@@ -10,6 +10,9 @@ class MozillaCachePlugin(JanitorCachePlugin):
     __category__ = 'application'
 
     targets = ['Cache',
+               'safebrowsing',
+               'startupCache',
+               'thumbnails',
                'OfflineCache']
     app_path = ''
 
@@ -24,12 +27,12 @@ class MozillaCachePlugin(JanitorCachePlugin):
                     if section.startswith('Profile'):
                         relative_id = config.get_value(section, 'IsRelative')
                         if relative_id == profile_id:
-                            return os.path.expanduser('%s/%s' % (cls.app_path, config.get_value(section, 'Path')))
+                            return os.path.expanduser('%s/%s' % (cls.cache_path, config.get_value(section, 'Path')))
             except Exception, e:
                 log.error(e)
                 path = config.get_value('Profile0', 'Path')
                 if path:
-                    return os.path.expanduser('%s/%s' % (cls.app_path, path))
+                    return os.path.expanduser('%s/%s' % (cls.cache_path, path))
         return cls.root_path
 
 
@@ -37,9 +40,11 @@ class FirefoxCachePlugin(MozillaCachePlugin):
     __title__ = _('Firefox Cache')
 
     app_path = '~/.mozilla/firefox'
+    cache_path = '~/.cache/mozilla/firefox'
 
 
 class ThunderbirdCachePlugin(MozillaCachePlugin):
     __title__ = _('Thunderbird Cache')
 
     app_path = '~/.thunderbird'
+    cache_path = '~/.cache/thunderbird'
